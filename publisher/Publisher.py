b5ba1cfba8cfd800f76b87af1b4f469503051559
Hugo Diniz <hhldiniz@gmail.com>
2018-04-23 20:14:38 -0300

    implementação do on_message do publisher

--- publisher/Publisher.py
+++ publisher/Publisher.py
@@ -4,4 +4,19 @@ from BaseMqttClass import BaseMqtt
 class Publisher(BaseMqtt):
     def __init__(self, saldo_inicial):
         super().__init__()
-        self.__saldo_inicial = saldo_inicial
+        self.__saldo = saldo_inicial
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def retirada(self, valor):
+        self.__saldo -= valor
+
+    def deposito(self, valor):
+        self.__saldo += valor
+
+    def on_message(self, client, userdata, message):
+        if message.topic == "saldo/retirada":
+            self.retirada(message.payload)
+        elif message.topic == "saldo/deposito":
+            self.deposito(message.payload)
