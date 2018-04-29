98ca9002175f8370521a98e0f1d5b27ce7275077
Hugo Diniz <hhldiniz@gmail.com>
2018-04-29 13:05:34 -0300

    publicação inicial

--- publisher/Publisher.py
+++ publisher/Publisher.py
@@ -5,18 +5,24 @@ class Publisher(BaseMqtt):
     def __init__(self, saldo_inicial):
         super().__init__()
         self.__saldo = saldo_inicial
+        self.publish_value("saldo/valor", self.__saldo)
+
+    def publish_value(self, topic, payload):
+        self.get_mqtt_client().publish(topic, payload)
 
     def get_saldo(self):
         return self.__saldo
 
-    def retirada(self, valor):
-        self.__saldo -= valor
+    def withdraw(self, value):
+        self.__saldo -= value
+        self.publish_value("saldo/valor", value)
 
-    def deposito(self, valor):
-        self.__saldo += valor
+    def deposit(self, value):
+        self.__saldo += value
+        self.publish_value("saldo/valor", value)
 
     def on_message(self, client, userdata, message):
         if message.topic == "saldo/retirada":
-            self.retirada(message.payload)
+            self.withdraw(message.payload)
         elif message.topic == "saldo/deposito":
-            self.deposito(message.payload)
+            self.deposit(message.payload)
